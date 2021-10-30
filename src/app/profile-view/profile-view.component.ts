d1e2cdde614906bbf485bb9b8490d7cf42e31698
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-10-29 21:55:29 -0400

    -profile-view built

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatCard } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-profile-view',
@@ -6,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-view.component.scss'],
 })
 export class ProfileViewComponent implements OnInit {
-  constructor() {}
+  @Input() userData = {
+    Name: '',
+    Username: '',
+    Birthdate: '',
+    NewPassword: '',
+    ConfirmPassword: '',
+  };
 
-  ngOnInit(): void {}
+  user: any = {};
+  token: string = '';
+  constructor(public fetchApiData: UserRegistrationService) {}
+
+  ngOnInit(): void {
+    const user = localStorage.getItem('user');
+    if (typeof user === 'string') {
+      const parse = JSON.parse(user);
+      this.user = parse;
+    }
+
+    const token: any = localStorage.getItem('token');
+    this.token = token;
+  }
 }
